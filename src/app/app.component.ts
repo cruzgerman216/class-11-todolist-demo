d7054bb3c979b07c4f4814ca9b67ac9a4567f279
German Cruz <43277807+cruzgerman216@users.noreply.github.com>
2022-09-22 19:19:45 -0500

    Class 11 Demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import Todo from './shared/todolist.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'todolist-demo';
+  todolist: Todo[] = [
+    {
+      id: 1,
+      action: 'Dishes',
+      completed: true
+    },
+    {
+      id: 2,
+      action: 'Sweep',
+      completed: false,
+    },
+  ];
+
+
+  toggleComplete(index){
+    this.todolist[index].completed = !this.todolist[index].completed 
+  }
+  addTodo(){
+    let newTodo: Todo = {
+      id: this.todolist.length + 1, 
+      action: "Some chore",
+      completed: false
+    }
+
+    this.todolist.push(newTodo)
+  }
 }
---
+++ src/app/shared/todolist.model.ts
@@ -0,0 +1,5 @@
+export default interface Todo{
+  id: number;
+  action: string; 
+  completed: boolean
+}
